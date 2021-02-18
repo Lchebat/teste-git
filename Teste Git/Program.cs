3f5231a9bbb971b62f0998d619b1673713800610
Lucas Chebat <lucaschebat@gmail.com>
2021-02-18 06:21:01 -0300

    Acrescentada pergunta como voce esta

--- Teste Git/Program.cs
+++ Teste Git/Program.cs
@@ -5,6 +5,7 @@ namespace Teste_Git {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom Dia!");
+            Console.WriteLine("Como você esta hoje?");
             Console.ReadLine();
         }
     }
