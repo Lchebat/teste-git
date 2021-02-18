4b7d0cc32f1cac185d24a5d396ed74122bec7704
Lucas Chebat <lucaschebat@gmail.com>
2021-02-18 06:08:18 -0300

    Acrescentado mensagem de bom dia

--- Teste Git/Program.cs
+++ Teste Git/Program.cs
@@ -4,6 +4,8 @@ namespace Teste_Git {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia!");
+            Console.ReadLine();
         }
     }
 }
